ff93bbc56ce62a78533a0c6806fb72dc1600fdab
App Academy Student <student@appacademy.io>
2014-10-28 10:57:59 -0400

    make login template, add logout functionality, make dynamic navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,33 @@
 class User < ActiveRecord::Base
-  validates :email, :password, :session_token, presence: true
-  
+  validates :email, :password_digest, :session_token, presence: true
+
   after_initialize :ensure_session_token
-  
+
   attr_reader :password
-  
+
   def self.find_by_credentials(email, password)
-    user = User.find_by_email(email) 
+    user = User.find_by_email(email)
     return nil unless user && user.valid_password?(password)
     user
   end
-  
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
-  
+
   def valid_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
-  
+
   def reset_token!
     self.session_token = SecureRandom.urlsafe_base64(16)
     self.save!
     self.session_token
   end
-  
+
   private
-  
+
   def ensure_session_token
     self.session_token ||= SecureRandom.urlsafe_base64(16)
   end
