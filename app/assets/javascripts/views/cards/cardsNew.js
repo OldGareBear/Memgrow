388d210a26d5ac58537a639e7dd8b86655d38d8d
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2014-11-05 09:40:21 -0500

    get card sub forms working

--- app/assets/javascripts/views/cards/cardsNew.js
+++ app/assets/javascripts/views/cards/cardsNew.js
@@ -1,38 +1,59 @@
 Memgrow.Views.CardsNew = Backbone.View.extend({
 	template: JST['cards/new'],
-	
+
 	render: function() {
 		var content = this.template({
 			course: this.model
 		});
-		
+
 		this.$el.html(content);
-		
+
 		return this;
 	},
-	
+
 	events: {
 		"submit": "addCard"
 	},
-	
+
 	addCard: function(event) {
 		event.preventDefault();
 		var params = $(event.target).serializeJSON();
 		var character = params["card"]["character"];
 		var pinyin = params["card"]["pinyin"];
 		var english = params["card"]["english"];
-		
+
 		var newCard = new Memgrow.Models.Card({
 			character: character,
 			pinyin: pinyin,
 			english: english,
 			course_id: this.model.get("id")
 		});
-		
+
+		var that = this;
+
 		newCard.save({}, {
 			success: function() {
 				console.log("card saved")
+				// append the next card form onto the bottom of the old one
+				that.appendSubform()
 			}
 		});
+	},
+
+	appendSubform: function() {
+		$("div.new-card").append(JST['cards/subForm'])
 	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+//scroll past bottom
